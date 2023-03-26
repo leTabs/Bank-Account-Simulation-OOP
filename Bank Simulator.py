8bcf4a16f127ee9df18cda3d837465f3071178e9
leTabs <125308756+leTabs@users.noreply.github.com>
2023-03-26 22:20:38 +0300

    Update Bank Simulator.py

--- Bank Simulator.py
+++ Bank Simulator.py
@@ -1,7 +1,24 @@
-import random
+
+print('-'*30)
+print('Welcome to the "Imagenary Bank"')
+print(' ')
+print('-'*30)
+'''
+while True:
+    print('Choose one of the followings:')
+    print('1) Create account')
+    print('2) Log in ')
+    print('3) See more information'
+    print('='*30)
+    choice = int(input('Enter here:'))
+    if choice == 3:
+        print('The bank\'s interest rate is 2% per anoum')
+        break
+    else: break
+'''
 
 class Account:
-    balance = 100
+    balance = 300
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -17,14 +34,13 @@ class Account:
             print (f'Maximun possible withdrawl amount: {cls.balance}')
             return '_'
         cls.balance -= w_amount
-        
+    @classmethod
+    def view_interest_rate(cls, years):
+        return f'The interest in {years} years is: {(cls.balance * 0.02) * years}$'
     @classmethod
     def get_balance(cls):
         print(cls.balance)
     
     
-a1 = Account('Mark', 23)
-a1.deposit(340)
-a1.get_balance()
-a1.withdrawl(540)
-a1.get_balance()
+a1 = Account('john', 35)
+print(a1.view_interest_rate(3))
