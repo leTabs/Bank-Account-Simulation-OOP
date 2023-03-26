ec2b401d22f5dc24fa66d82e2e24ea6ca701f858
leTabs <125308756+leTabs@users.noreply.github.com>
2023-03-26 22:01:01 +0300

    Update Bank Simulator.py

--- Bank Simulator.py
+++ Bank Simulator.py
@@ -1,18 +1,30 @@
 import random
 
 class Account:
-
-    balance = 10
-
+    balance = 100
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
+        
     @classmethod
-    def deposit(cls, d_pop):
-        cls.balance -= 8
-
+    def deposit(cls, d_amount):
+        cls.balance += d_amount
+    
+    @classmethod
+    def withdrawl(cls, w_amount):
+        if w_amount > cls.balance:
+            print('Insafisant funds')
+            print (f'Maximun possible withdrawl amount: {cls.balance}')
+            return '_'
+        cls.balance -= w_amount
+        
     @classmethod
     def get_balance(cls):
         print(cls.balance)
-
+    
+    
+a1 = Account('Mark', 23)
+a1.deposit(340)
+a1.get_balance()
+a1.withdrawl(540)
+a1.get_balance()
